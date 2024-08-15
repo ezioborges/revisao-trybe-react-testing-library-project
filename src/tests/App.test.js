7030f10fde119c435fa9879c4ee39d2add0e51c2
Ezio <eziopborges@gmail.com>
2024-08-15 15:50:17 -0300

    verifica se ao clicar em Home redireciona para a pagina inicial

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -9,5 +10,16 @@ describe('Teste se o topo da aplicação contém um conjunto fixo de links de na
     expect(screen.getByRole('link', { name: /Home/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /About/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /Favorite Pokémon/i })).toBeInTheDocument();
+  });
+
+  test(`Teste se a aplicação é 
+    redirecionada para a página inicial, na URL / 
+    ao clicar no link Home da barra de navegação`, async () => {
+    renderWithRouter(<App />, { route: '/about' });
+
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    await userEvent.click(homeLink);
+
+    expect(screen.getByText(/Pokédex/i)).toBeInTheDocument();
   });
 });
