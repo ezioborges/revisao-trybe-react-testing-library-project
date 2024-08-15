43c81fcd66d512488176a9112cc6f61839bdeb87
Ezio <eziopborges@gmail.com>
2024-08-15 16:12:05 -0300

    verifica se ao clicar em "Favotite Pokemon" direciona para a página Favorite Pokémon

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
@@ -21,5 +21,27 @@ describe('Teste se o topo da aplicação contém um conjunto fixo de links de na
     await userEvent.click(homeLink);
 
     expect(screen.getByText(/Pokédex/i)).toBeInTheDocument();
+  });
+
+  test(`Teste se a aplicação é redirecionada 
+    para a página de About, na 
+    URL /about, ao clicar no link About da barra de navegação;`, async () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    await userEvent.click(aboutLink);
+
+    expect(screen.getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+
+  test(`Teste se a aplicação é redirecionada para 
+    a página de Pokémon Favoritados, na URL /favorites, ao clicar no 
+    link Favorite Pokémon da barra de navegação`, async () => {
+    renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
+    await userEvent.click(favoriteLink);
+
+    expect(screen.getByText(/No favorite Pokémon found/i)).toBeInTheDocument();
   });
 });
