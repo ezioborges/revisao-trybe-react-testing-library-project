d12609866d3a75b14a1bc1d883a0817a9672bde9
Ezio <eziopborges@gmail.com>
2024-08-15 15:33:40 -0300

    Iniciando! Testa o topo da aplicação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  test('O primeiro link deve possuir o texto "Home"', () => {
+    renderWithRouter(<App />);
+
+    expect(screen.getByRole('link', { name: /Home/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /About/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /Favorite Pokémon/i })).toBeInTheDocument();
+  });
+});
