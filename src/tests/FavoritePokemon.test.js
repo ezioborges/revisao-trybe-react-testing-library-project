91fd22b283a34350c2eb63f9dd8c6c19d73daa6a
Ezio <eziopborges@gmail.com>
2024-08-16 14:39:19 -0300

    Testa se é exibido o testo "No favorite pokemon found" quando acessa a pagina sem favoritar nenhum pokemon

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente <FavoritePokemon.js />', () => {
+  test('Testa se é exibida na tela a mensagem "No favorite pokemon found", caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+
+    // const title = ;
+
+    expect(screen.getByText(/No favorite Pokémon found/i)).toBeInTheDocument();
+  });
+});
