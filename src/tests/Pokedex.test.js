642747752a9f512a32476291e7fa916cc5b5ae32
Ezio <eziopborges@gmail.com>
2024-08-16 16:10:09 -0300

    Testa o componente Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto "Encountered Pokémon"', () => {
+    renderWithRouter(<App />);
+
+    expect(screen.getByText(/Encountered Pokémon/i)).toBeInTheDocument();
+  });
+});
+
+describe('Teste se é exibido o próximo Pokémon da lista quando o botão "Próximo Pokémon" é clicado', () => {
+  test('O botão deve conter o texto Próximo Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const buttonNext = screen.getByRole('button', { name: /Próximo Pokémon/i });
+
+    expect(buttonNext).toBeInTheDocument();
+  });
+
+  test('Testa os botões de filtragem', () => {
+    renderWithRouter(<App />);
+
+    const fireBtn = screen.getByRole('button', { name: /Fire/i });
+
+    const totalBtns = screen.getAllByRole('button');
+    const btnsTestid = screen.getAllByTestId('pokemon-type-button');
+
+    expect(fireBtn).toBeInTheDocument();
+    expect(totalBtns).toHaveLength(9);
+    expect(btnsTestid).toHaveLength(7);
+  });
+
+  test('testando "PokemonButtonPAnel"', () => {
+    renderWithRouter(<App />);
+  });
+});
